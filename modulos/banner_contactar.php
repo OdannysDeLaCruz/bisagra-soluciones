659cfe58eef9538f1529940f721f8d48ef23fae0
Odannys De La Cruz <el_odanis321@hotmail.com>
2020-12-20 13:17:05 -0500

    Reorganización de archivos

--- modulos/banner_contactar.php
+++
@@ -1,8 +0,0 @@
-<!-- BANNER CONTACTAR-->
-<section class="banner row">
-	<p class="col-xs-12 col-md-5 offset-md-1 banner_texto">¿Que esperas? contactanos para contratar un trabajador. </p>
-	<a href="/contacto.php" class="col-xs-12 col-md-6 banner_link">
-		<button class="btn btn-primary boton_defauld">Contactar</button>
-	</a>
-</section>
-<!-- FIN BANNER CONTACTAR-->
